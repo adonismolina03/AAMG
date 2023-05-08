63bc0c54cfa63c0d74104291bf11802cee4a170f
adonismolina03 <adonismolina03@gmail.com>
2023-05-08 16:15:49 -0600

    campos nombre y dueño con validacion

--- AAMG.AppMVC/Documentos/AAMGBD.sql
+++ AAMG.AppMVC/Documentos/AAMGBD.sql
@@ -2,10 +2,10 @@ CREATE DATABASE AAMGDB
 USE AAMGDB
 CREATE TABLE mascotas(
 Id int identity(1,1) primary key,
-Nombre varchar(50),
+Nombre varchar(50) not null,
 Tipo varchar(50),
 Raza varchar(50),
 Edad int,
-Dueño varchar(50),
+Dueño varchar(50) not null,
 Antecedentes varchar(300)
 )
--- AAMG.AppMVC/Models/Mascota.cs
+++ AAMG.AppMVC/Models/Mascota.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Build.Framework;
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 
 namespace AAMG.AppMVC.Models;
@@ -6,16 +8,15 @@ namespace AAMG.AppMVC.Models;
 public partial class Mascota
 {
     public int Id { get; set; }
-
-    public string? Nombre { get; set; }
-
+    [System.ComponentModel.DataAnnotations.Required] public string? Nombre { get; set; }
     public string? Tipo { get; set; }
+
 
     public string? Raza { get; set; }
 
     public int? Edad { get; set; }
 
-    public string? Dueño { get; set; }
+    [System.ComponentModel.DataAnnotations.Required] public string? Dueño { get; set; }
 
     public string? Antecedentes { get; set; }
 }
